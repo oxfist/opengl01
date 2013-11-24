85f6944f172a824a76476e81c2c9758fdae6ff68
Andrés Caro <andres.caro.q@gmail.com>
2013-11-23 21:25:05 -0300

    Changed vertex from 3f to 2f.

--- test01.cpp
+++ test01.cpp
@@ -4,12 +4,20 @@ void display(void) {
     glClearColor(1,0,0,0);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glBegin(GL_POINTS);
+        glVertex2f(0.0, 0.0);
+        glVertex2f(0.5, 0.0);
+        glVertex2f(0.5, 0.5);
+        glVertex2f(0.0, 0.5);
+    glEnd();
+    /*
     glBegin(GL_POLYGON);
         glVertex3f(0.0, 0.0, 0.0);
         glVertex3f(0.5, 0.0, 0.0);
         glVertex3f(0.5, 0.5, 0.0);
         glVertex3f(0.0, 0.5, 0.0);
     glEnd();
+    */
 
     glFlush();
 }
